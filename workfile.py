a94e644f493b68198b0777221454d2b042727853
swaption2009 <swaption2009@gmail.com>
2016-12-10 00:43:01 -0800

    training/test data split

--- workfile.py
+++ workfile.py
@@ -38,3 +38,74 @@ print "Graduation rate of the class: {:.2f}%".format(grad_rate)
 
 # PREPARE DATA
 
+# Extract feature columns
+feature_cols = list(student_data.columns[:-1])
+
+# Extract target column 'passed'
+target_col = student_data.columns[-1]
+
+# Show the list of columns
+print "Feature columns:\n{}".format(feature_cols)
+print "\nTarget column: {}".format(target_col)
+
+# Separate the data into feature data and target data (X_all and y_all, respectively)
+X_all = student_data[feature_cols]
+y_all = student_data[target_col]
+
+# Show the feature information by printing the first five rows
+print "\nFeature values:"
+print X_all.head()
+
+
+# PREPROCESSING (using Panda get_dummies)
+
+def preprocess_features(X):
+    ''' Preprocesses the student data and converts non-numeric binary variables into
+        binary (0/1) variables. Converts categorical variables into dummy variables. '''
+
+    # Initialize new output DataFrame
+    output = pd.DataFrame(index=X.index)
+
+    # Investigate each feature column for the data
+    for col, col_data in X.iteritems():
+
+        # If data type is non-numeric, replace all yes/no values with 1/0
+        if col_data.dtype == object:
+            col_data = col_data.replace(['yes', 'no'], [1, 0])
+
+        # If data type is categorical, convert to dummy variables
+        if col_data.dtype == object:
+            # Example: 'school' => 'school_GP' and 'school_MS'
+            col_data = pd.get_dummies(col_data, prefix=col)
+
+            # Collect the revised columns
+        output = output.join(col_data)
+
+    return output
+
+
+X_all = preprocess_features(X_all)
+print "Processed feature columns ({} total features):\n{}".format(len(X_all.columns), list(X_all.columns))
+
+# Train/test data split
+
+# TODO: Import any additional functionality you may need here
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=0.25, random_state=42)
+
+# TODO: Set the number of training points
+num_train = X_train.shape[0]
+
+# Set the number of testing points
+num_test = X_all.shape[0] - num_train
+
+# TODO: Shuffle and split the dataset into the number of training and testing points above
+X_train = X_train
+X_test = X_test
+y_train = y_train
+y_test = y_test
+
+# Show the results of the split
+print "Training set has {} samples.".format(X_train.shape[0])
+print "Testing set has {} samples.".format(X_test.shape[0])
