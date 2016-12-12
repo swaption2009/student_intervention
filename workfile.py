6ae686dd10b88fbf6890b705c0c694a3f7d9f03d
swaption2009 <swaption2009@gmail.com>
2016-12-11 23:04:41 -0800

    Implementation: Model Performance Metrics

--- workfile.py
+++ workfile.py
@@ -109,3 +109,92 @@ y_test = y_test
 # Show the results of the split
 print "Training set has {} samples.".format(X_train.shape[0])
 print "Testing set has {} samples.".format(X_test.shape[0])
+
+
+# SETUP
+
+def train_classifier(clf, X_train, y_train):
+    ''' Fits a classifier to the training data. '''
+
+    # Start the clock, train the classifier, then stop the clock
+    start = time()
+    clf.fit(X_train, y_train)
+    end = time()
+
+    # Print the results
+    print "Trained model in {:.4f} seconds".format(end - start)
+
+
+def predict_labels(clf, features, target):
+    ''' Makes predictions using a fit classifier based on F1 score. '''
+
+    # Start the clock, make predictions, then stop the clock
+    start = time()
+    y_pred = clf.predict(features)
+    end = time()
+
+    # Print and return results
+    print "Made predictions in {:.4f} seconds.".format(end - start)
+    return f1_score(target.values, y_pred, pos_label='yes')
+
+
+def train_predict(clf, X_train, y_train, X_test, y_test):
+    ''' Train and predict using a classifer based on F1 score. '''
+
+    # Indicate the classifier and the training set size
+    print "Training a {} using a training set size of {}. . .".format(clf.__class__.__name__, len(X_train))
+
+    # Train the classifier
+    train_classifier(clf, X_train, y_train)
+
+    # Print the results of prediction for both training and testing
+    print "F1 score for training set: {:.4f}.".format(predict_labels(clf, X_train, y_train))
+    print "F1 score for test set: {:.4f}.".format(predict_labels(clf, X_test, y_test))
+
+
+
+# 3 classifiers: logistics regression, naive bayes, and decision tree
+
+# TODO: Import the three supervised learning models from sklearn
+# from sklearn import model_A
+from sklearn import linear_model
+# from sklearn import model_B
+from sklearn.naive_bayes import MultinomialNB
+# from skearln import model_C
+from sklearn.tree import DecisionTreeClassifier
+
+# TODO: Initialize the three models
+clf_A = linear_model.LogisticRegression()
+clf_B = MultinomialNB()
+clf_C = DecisionTreeClassifier(random_state=0)
+
+# TODO: Set up the training set sizes
+X_train_100, X_test_100, y_train_100, y_test_100 = train_test_split(X_all, y_all, test_size=0.75, random_state=42)
+X_train_100 = X_train_100
+y_train_100 = y_train_100
+
+X_train_200, X_test_200, y_train_200, y_test_200 = train_test_split(X_all, y_all, test_size=0.5, random_state=42)
+X_train_200 = X_train_200
+y_train_200 = y_train_200
+
+X_train_300, X_test_300, y_train_300, y_test_300 = train_test_split(X_all, y_all, test_size=0.25, random_state=42)
+X_train_300 = X_train_300
+y_train_300 = y_train_300
+
+# TODO: Execute the 'train_predict' function for each classifier and each training set size
+
+# set test_size of 0.25 (296 training data)
+train_predict(clf_A, X_train_100, y_train_100, X_test_100, y_test_100)
+train_predict(clf_B, X_train_100, y_train_100, X_test_100, y_test_100)
+train_predict(clf_C, X_train_100, y_train_100, X_test_100, y_test_100)
+
+# set test_size of 0.5 (296 training data)
+train_predict(clf_A, X_train_200, y_train_200, X_test_200, y_test_200)
+train_predict(clf_B, X_train_200, y_train_200, X_test_200, y_test_200)
+train_predict(clf_C, X_train_200, y_train_200, X_test_200, y_test_200)
+
+# set test_size of 0.75 (296 training data)
+train_predict(clf_A, X_train_300, y_train_300, X_test_300, y_test_300)
+train_predict(clf_B, X_train_300, y_train_300, X_test_300, y_test_300)
+train_predict(clf_C, X_train_300, y_train_300, X_test_300, y_test_300)
+
